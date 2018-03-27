c782a2e020b3479875a37189385d34822bb2a048
mawandong1993 <mawandong1993@foxmail.com>
2018-03-28 01:30:25 +0800

    更新代码

---
+++ common/src/main/java/utils/DateUtils.java
@@ -0,0 +1,13 @@
+package utils;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author mawandong
+ * @date 2018/3/28 1:28
+ */
+public class DateUtils {
+
+    private final static Logger Log = LoggerFactory.getLogger(DateUtils.class);
+}
