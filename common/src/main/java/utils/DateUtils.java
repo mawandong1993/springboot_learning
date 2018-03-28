d82eb7cc2a50360cc85e3de8de8798ad084364b6
mawandong1993 <mawandong1993@foxmail.com>
2018-03-29 00:44:54 +0800

    更新代码

--- common/src/main/java/utils/DateUtils.java
+++ common/src/main/java/utils/DateUtils.java
@@ -1,7 +1,11 @@
 package utils;
 
+import org.apache.commons.lang3.time.DateFormatUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.Date;
+
 
 /**
  * @author mawandong
@@ -10,4 +14,14 @@ import org.slf4j.LoggerFactory;
 public class DateUtils {
 
     private final static Logger Log = LoggerFactory.getLogger(DateUtils.class);
+
+    private final static String DATEFORMAT = "yyyy-MM-dd HH:mm:ss";
+
+    private static String format(){
+        return DateFormatUtils.format(new Date(),DateUtils.DATEFORMAT);
+    }
+    private static String format(Date date,String pattern){
+        return DateFormatUtils.format(date,pattern);
+    }
+
 }
---
+++ common/src/main/java/utils/FileUtils.java
@@ -0,0 +1,9 @@
+package utils;
+
+/**
+ * @author mawandong
+ * @date 2018/3/29 0:44
+ */
+public class FileUtils {
+
+}
