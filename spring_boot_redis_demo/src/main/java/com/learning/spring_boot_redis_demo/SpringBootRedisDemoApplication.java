2e8f60870c55e4cb411fa95a58770691443adfa9
mawandong1993 <mawandong1993@foxmail.com>
2018-06-15 01:00:52 +0800

    代码更新

---
+++ spring_boot_redis_demo/src/main/java/com/learning/spring_boot_redis_demo/SpringBootRedisDemoApplication.java
@@ -0,0 +1,12 @@
+package com.learning.spring_boot_redis_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRedisDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootRedisDemoApplication.class, args);
+    }
+}
