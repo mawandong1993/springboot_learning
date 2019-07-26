0981a4290468159267d374a5f11658b14d6ce591
mawandong1993 <mawandong1993@foxmail.com>
2019-07-26 23:50:25 +0800

    调整结构

--- spring_boot_in_action/spring_boot_swagger2/src/main/java/com/learning/spring_boot_swagger2/config/Swagger2.java
+++ spring_boot_in_action/spring_boot_swagger2/src/main/java/com/learning/spring_boot_swagger2/config/Swagger2.java
@@ -1,4 +1,4 @@
-package com.learning.spring_boot_swagger2;
+package com.learning.spring_boot_swagger2.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
