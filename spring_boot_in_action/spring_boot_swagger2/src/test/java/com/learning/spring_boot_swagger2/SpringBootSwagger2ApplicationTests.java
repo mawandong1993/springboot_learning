c6f7baa0c81906503dda1b1c50082162c53eddc4
mawandong1993 <mawandong1993@foxmail.com>
2021-07-11 14:10:21 +0800

    更新依赖版本

--- spring_boot_in_action/spring_boot_swagger2/src/test/java/com/learning/spring_boot_swagger2/SpringBootSwagger2ApplicationTests.java
+++ spring_boot_in_action/spring_boot_swagger2/src/test/java/com/learning/spring_boot_swagger2/SpringBootSwagger2ApplicationTests.java
@@ -1,16 +1,21 @@
 package com.learning.spring_boot_swagger2;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+@Slf4j
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = SpringBootSwagger2Application.class)
 public class SpringBootSwagger2ApplicationTests {
 
     @Test
     public void contextLoads() {
+        if (1 >= 2) {
+            log.info("");
+        }
     }
 
 }
